1ee73de0f002f8149835aced994af4898173f3e8
inzee86333 <56289715+inzee86333@users.noreply.github.com>
2020-09-17 14:50:31 +0700

    commit

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
+import MyButton from './components/myButton.js';
 
 export default function App() {
+  const [searchInput, setInput] = useState('')
+  const [color, setColor] = useState('red')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>สวัสดีชาวโลก</Text>
-      <StatusBar style="auto" />
+      <Image 
+      source = {{uri:'https://www.google.co.th/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'}}
+      style={styles.logo}
+      />
+      <Text 
+      style={styles.title}>Google</Text>
+      <TextInput 
+      value={searchInput}
+      onChangeText ={(text)=>{ setInput(text)}}
+      style={styles.searchInput} 
+      placeholder="ค้นหา"
+      ></TextInput>
+      <Text style={{fontSize: 18, marginTop: 20, color: color}}>{searchInput}</Text>
+      <View style={styles.buttonLayout}>
+        <MyButton title = 'red' color='red' onChangeColor={(color) => {setColor(color)}} />
+        <MyButton title = 'blue' color='blue' onChangeColor={(color) => {setColor(color)}} />
+        <MyButton title = 'green' color='green' onChangeColor={(color) => {setColor(color)}} />
+      </View>
     </View>
   );
 }
@@ -18,5 +36,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  },logo: {
+    width: 200,
+    height: 100,
+    resizeMode: 'contain',
+  },title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'gray',
+    marginTop: 20,
+  },searchInput:{
+    height: 45,
+    width: '70%',
+    borderColor: 'gray',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 25,
+    marginTop: 10,
+    paddingLeft: 20,
+    shadowColor: "#000",
+  },buttonLayout: {
+    width: '50%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 20,
+  }
 });
---
+++ components/myButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Button} from 'react-native';
+export default (props) =>{
+    return<Button 
+    title={props.title}
+    color={props.color} 
+    onPress={()=>{props.onChangeColor(props.color)}}/>
+}
